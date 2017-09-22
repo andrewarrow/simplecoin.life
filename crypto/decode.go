da26530b1be32c53168fc8eb144e4ab851dd4288
A Arrow <oneone@gmail.com>
2017-09-22 19:21:21 +0000

    better

--- crypto/decode.go
+++ crypto/decode.go
@@ -1,21 +1,19 @@
 package crypto
 
-import "fmt"
-
+//import "fmt"
 //import "encoding/binary"
 import "math/big"
 
-func Decode(bc big.Int, n, d int64) string {
+func Decode(bc big.Int, n, d uint64) big.Int {
 
 	//Plaintext = Cd mod n
-	bd := big.NewInt(d)
-	bn := big.NewInt(n)
-	fmt.Println("-bc", bc, bd, bn)
+	var bd big.Int
+	var bn big.Int
+	bd.SetUint64(d)
+	bn.SetUint64(n)
+	//fmt.Println("-bc", bc, bd, bn)
 
 	var limit big.Int
-	p := limit.Exp(&bc, bd, bn)
-
-	fmt.Println(p)
-
-	return ""
+	p := limit.Exp(&bc, &bd, &bn)
+	return *p
 }
--- main.go
+++ main.go
@@ -10,16 +10,20 @@ func main() {
 	//n, e, d := crypto.GenKeys()
 	n := uint64(828719)
 	e := uint64(412211)
-	//d := uint64(252347)
+	d := uint64(252347)
 
 	n = uint64(91)
 	e = uint64(5)
-	//d = int64(29)
+	d = uint64(29)
 
 	//fmt.Println(e, n, d)
 	//crypto.Encode("hello", 828719, 412211)
 	//crypto.Encode("hello", 828719, 412211)
 	c := crypto.Encode(10, n, e)
-	fmt.Println(c)
+	fmt.Println("10*", c, crypto.Decode(c, n, d))
+	c = crypto.Encode(90, n, e)
+	fmt.Println("90*", c, crypto.Decode(c, n, d))
+	c = crypto.Encode(89, n, e)
+	fmt.Println("89*", c, crypto.Decode(c, n, d))
 	//crypto.Decode(c, n, d)
 }
