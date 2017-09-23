fe2fd7e02a58804a723c1c0cdef2feb90f55fd2b
A Arrow <oneone@gmail.com>
2017-09-23 18:43:10 +0000

    sig

--- crypto/transaction.go
+++ crypto/transaction.go
@@ -3,13 +3,15 @@ package crypto
 import "encoding/json"
 
 type Transaction struct {
+	Index                   uint64   `json:"index"`
 	Hash                    string   `json:"hash"`
 	OwnersPublicAccount     []uint64 `json:"owners_public_account"`
 	PreviousOwnersSignature []uint64 `json:"previous_owners_signature"`
 }
 
-func NewTransaction() Transaction {
+func NewTransaction(index int) Transaction {
 	t := Transaction{}
+	t.Index = uint64(index)
 	return t
 }
 func (t Transaction) Encode() string {
--- main.go
+++ main.go
@@ -11,18 +11,42 @@ func main() {
 	//n := uint64(828719)
 	//e := uint64(412211)
 	//d := uint64(252347)
+	/*
+		n0 := uint64(91)
+		e0 := uint64(5)
+		d0 := uint64(29)
+	*/
+	n1 := uint64(44817313)
+	e1 := uint64(22946869)
+	d1 := uint64(34788097)
 
-	n := uint64(44817313)
-	e := uint64(22946869)
-	d := uint64(34788097)
+	n2 := uint64(828719)
+	e2 := uint64(412211)
+	//d2 := uint64(252347)
 
-	fmt.Printf("Public Account Number: %d-%d\n", n, e)
-	fmt.Printf("Private Account Number: %d-%d\n", n, d)
+	/*
+		n3 := uint64(828719)
+		e3 := uint64(412211)
+		d3 := uint64(252347)
+	*/
 
-	t := crypto.NewTransaction()
-	t.Hash = "BLAH"
-	t.OwnersPublicAccount = []uint64{n, e}
-	fmt.Println(t.Encode())
+	fmt.Printf("Public Account Number: %d-%d\n", n1, e1)
+	fmt.Printf("Private Account Number: %d-%d\n", n1, d1)
+
+	// CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+	//t0 := crypto.NewTransaction(0)
+
+	t1 := crypto.NewTransaction(1)
+	t1.Hash = "1" + fmt.Sprintf(",%d-%d", n1, e1)
+	t1.OwnersPublicAccount = []uint64{n1, e1}
+	t1.PreviousOwnersSignature = []uint64{} // 0's sig of hash
+	fmt.Println(t1.Encode())
+
+	t2 := crypto.NewTransaction(2)
+	t2.Hash = t1.Hash + fmt.Sprintf(",%d-%d", n2, e2)
+	t2.OwnersPublicAccount = []uint64{n2, e2}
+	t2.PreviousOwnersSignature = []uint64{1, 2, 3} // 1's sig of hash
+	fmt.Println(t2.Encode())
 
 	/*
 		list := crypto.EncodeString("AHELLO", n, e)
