603218a3701f10fb688e613754e0a853278de058
A Arrow <oneone@gmail.com>
2017-10-09 14:39:28 +0000

    peer

--- peer/peer.go
+++ peer/peer.go
@@ -2,6 +2,8 @@ package peer
 
 import "fmt"
 import "net"
+import "io"
+import "bytes"
 
 func handleRequest(conn net.Conn) {
 	buff := make([]byte, 1024)
@@ -9,11 +11,22 @@ func handleRequest(conn net.Conn) {
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
+	fmt.Println(string(buff))
 	conn.Write([]byte("received."))
 	conn.Close()
 }
 
 func SayHello(peer string) {
+	conn, err := net.DialTimeout("tcp", peer, 9000*9000)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "HELLO SIMPLECOIN/0.1\n")
+	var buff bytes.Buffer
+	io.Copy(&buff, conn)
+	fmt.Println(string(buff.Bytes()))
 }
 
 func Listen(port string) {
